3d6a90b3db31533c3502b89a61aab8f31a862366
Ngô Tôn <ton.ngovan@liginc.vn>
2021-11-15 09:05:57 +0700

    Rewrite news year

--- wp/wp-content/themes/wp-exercise/functions/extend/99_path.php
+++ wp/wp-content/themes/wp-exercise/functions/extend/99_path.php
@@ -2,5 +2,6 @@
 add_action( 'init', 'add_news_rules' );
 function add_news_rules()
 {
-    add_rewrite_rule('news/([0-9]+)?$', 'index.php?post_type=news&p=$matches[1]', 'top');
+    //add_rewrite_rule('^news/([0-9]+)?$', 'index.php?post_type=news&p=$matches[1]', 'top');
+    add_rewrite_rule( '^news/([0-9]{4})/?', 'index.php?post_type=news&year=$matches[1]', 'top' );
 }
